3c314f185acf272130c12593eea3ba54aaffded4
danielnjord <danielnjord@hotmail.com>
2023-11-21 17:00:10 +0100

    ny

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 
 
-
-
-// SKAPA github och invite stefan.holmberg@systementor.se :D
-
-
     let dropDownButton = document.querySelector('.dropDown-btn')
     let dropDownContent = document.querySelector('.dropDown-content')
 
-   
-    // Set the selected duration when clicking on an option
-    let timeOption = document.querySelectorAll('.dropDown-content p')
+       let timeOption = document.querySelectorAll('.dropDown-content p')
     timeOption.forEach(function (option) {
         option.addEventListener('click', function () {
             let selectedDuration = option.innerHTML;
@@ -20,20 +13,14 @@
         })
     })
 
-    
-
     //------------------------------------------------------------------------------------------------------------------------
 
 
-
     document.addEventListener('DOMContentLoaded', function () {
-        // Get all elements with class 'view-front-view'
         let frontViews = document.querySelectorAll('.view-front-view');
     
-        // Add click event listener to each 'view-front-view'
         frontViews.forEach(function (frontView) {
             frontView.addEventListener('click', function () {
-                // Toggle the display property of the corresponding 'view-back-view'
                 let backView = frontView.nextElementSibling; // Assuming it's the next sibling
                 if (backView) {
                     backView.style.display = (backView.style.display === 'none' || backView.style.display === '') ? 'block' : 'none';
