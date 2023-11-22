e99c954510c00790d40dd23604f7656c683b700c
danielnjord <danielnjord@hotmail.com>
2023-11-22 12:18:41 +0100

    nice

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 
+NiceSelect.bind(document.getElementById("dropDown"))
 
-    let dropDownButton = document.querySelector('.dropDown-btn')
-    let dropDownContent = document.querySelector('.dropDown-content')
 
-       let timeOption = document.querySelectorAll('.dropDown-content p')
-    timeOption.forEach(function (option) {
-        option.addEventListener('click', function () {
-            let selectedDuration = option.innerHTML;
-            console.log('Selected time:', selectedDuration);
-            console.log("hej")
-            dropDownContent.classList.remove('show'); 
+document.addEventListener('DOMContentLoaded', function () {
+    let frontViews = document.querySelectorAll('.view-front-view')
+    
+    frontViews.forEach(function (frontView) {
+        frontView.addEventListener('click', function () {
+            let backView = frontView.nextElementSibling
+            if (backView) {
+                backView.style.display = (backView.style.display === 'none' 
+                || backView.style.display === '') ? 'block' : 'none'
+            }
         })
     })
+})
 
 //------------------------------------------------------------------------------------------------------------------------
 
+let cardClick = document.querySelectorAll('.card')
+let clickCount = 0
 
-    document.addEventListener('DOMContentLoaded', function () {
-        let frontViews = document.querySelectorAll('.view-front-view');
-    
-        frontViews.forEach(function (frontView) {
-            frontView.addEventListener('click', function () {
-                let backView = frontView.nextElementSibling;
-                if (backView) {
-                    backView.style.display = (backView.style.display === 'none' || backView.style.display === '') ? 'block' : 'none';
-                }
-            });
-        });
+// en loop som går igenom alla element som är med i klassen 'card' och lägger till en eventlistener på dem
+cardClick.forEach(function (card) {
+    card.addEventListener('click', function () {
+        clickCount++;
+        // uppdaterar textContent på elementet 'flips'
+        document.getElementById('antalKlick').textContent = clickCount;
     });
-
-//------------------------------------------------------------------------------------------------------------------------
-
+});
 
 
-    // Vill lägga en eventlistener på en class som alla cards är med i som räknar klicken
